4edbcf7798eed6794cfa55b953fbe316d1c3e5af
Manuel Díaz Pastor <116881211+manuelDiazpas@users.noreply.github.com>
2023-06-11 18:46:31 +0200

    Update ataviada-queries.sql
    Corrección de queries

--- ataviada-queries.sql
+++ ataviada-queries.sql
@@ -3,71 +3,66 @@ use ataviada;
 -- 1. Consultas
 
 -- Obtener el identificador de los clientes que han comprado algún producto
--- que no tenga una direccion
+-- que no tenga telefono
+select p.Cliente_codigo_cliente from pedido p 
+inner join cliente c ON p.Cliente_codigo_cliente = c.codigo_cliente
+where c.telefono is null;
 
-select c.CLIENTE_codigo_cliente from clientehacepedido c 
-where c.CLIENTE_codigo_cliente  in (select c2.codigo_cliente from cliente c2 
-where c2.direccion is null);
+-- ordenar los productos del más caro al más barato que sea menor de 100€ y mayor de 50€
+select p.nombre_producto, concat(p.precio, ' €') from producto p
+where p.precio between 50.0 and 100.0
+order by p.precio desc;
 
-
--- ordenar los productos del más caro al más barato que sea menor de 1000€ y mayor de 500€
-select p.nombre_producto, concat(p.precio, '€')  from modo_pago mp 
-inner join producto p on p.MODO_PAGO_codigo_cuenta = mp.codigo_cuenta 
-where p.precio between 500 and 1000
-order by p.precio desc; 
-
--- nombre de 5 productos reservados que valgan menos de 10€, ademas de la cantidad reservada
-select p.nombre_producto, concat(pr.precio, '€'), pr.cantidad  from productoaparecereserva pr 
-inner join producto p ON pr.PRODUCTO_cod_producto = p.cod_producto
-where pr.precio <10
-limit 5
+-- nombre de 5 productos pedidos que valgan menos de 10€, ademas de el precio total por todas las unidades pedidas
+select p.nombre_producto, lp.cantidad, lp.precio_unidad, concat(lp.cantidad*lp.precio_unidad) as 'Precio total' from linea_pedido lp 
+inner join producto p ON lp.Producto_codigo_producto = p.codigo_producto
+where lp.precio_unidad <10
+limit 5;
 
 -- productos que esten en stock agrupadas en tallas
-select s.talla, count(s.PRODUCTO_cod_producto) from stock s 
-inner join producto p ON s.PRODUCTO_cod_producto = p.cod_producto
-group by s.talla;
+select s.talla, count(p.codigo_producto) from stock s 
+inner join producto p ON s.talla = p.Stock_talla
+group by p.Stock_talla;
 
--- nombre de cliente que alquila producto + la fecha que se alquiló
-select c.nombre, a.fecha_alquiler, p.nombre_producto from alquila a 
-inner join cliente c ON c.codigo_cliente = a.CLIENTE_codigo_cliente 
-inner join producto p ON a.PRODUCTO_cod_producto = p.cod_producto ;
-
+-- nombre de cliente que reserva un producto en stock + la fecha que se reservó
+select c.codigo_cliente, r.fecha_inicio from reserva r 
+inner join cliente c ON r.Cliente_codigo_cliente = c.codigo_cliente;
 
 -- 2. Vistas
-
+-- vista de la consulta número 4
 create or replace view tallas_productos_stock
-as select s.talla, count(s.PRODUCTO_cod_producto) from stock s 
-inner join producto p ON s.PRODUCTO_cod_producto = p.cod_producto
-group by s.talla;
+as select s.talla, count(p.codigo_producto) from stock s 
+inner join producto p ON s.talla = p.Stock_talla
+group by p.Stock_talla;
 
-create or replace view clientes_sin_direccion
-as select c.CLIENTE_codigo_cliente from clientehacepedido c 
-where c.CLIENTE_codigo_cliente  in (select c2.codigo_cliente from cliente c2 
-where c2.direccion is null);
+-- vista de la consulta número 1
+create or replace view clientes_sin_telefono
+as select p.Cliente_codigo_cliente from pedido p 
+inner join cliente c ON p.Cliente_codigo_cliente = c.codigo_cliente
+where c.telefono is null;
 
 -- 3. Funciones y Procedimientos
 
 -- 3.1 Funciones
 
--- Funcion que te suma el precio total de dos productos en stock
+-- Funcion que te suma el precio total de una unidad de dos productos pedidos
 
 drop function suma;
 
 delimiter &&
 create Function Suma(p1 int, p2 int)
-returns double
+returns decimal
 deterministic
 begin
 	
-	declare pr1 double;
-	declare pr2 double;
+	declare pr1 decimal;
+	declare pr2 decimal;
 
-	select l.precio into pr1 from lineapedido l 
-	where l.PRODUCTO_cod_producto = p1;
-	
-	select l.precio into pr2 from lineapedido l 
-	where l.PRODUCTO_cod_producto = p2;
-	
+	select lp.precio_unidad into pr1 from linea_pedido lp
+	where lp.Producto_codigo_producto = p1;
+
+	select lp.precio_unidad into pr2 from linea_pedido lp
+	where lp.Producto_codigo_producto = p2;
 	
  Return (pr1 + pr2);
 end
@@ -75,7 +70,7 @@ end
 
 
 delimiter ;
-select suma(1, 100);
+select suma(1, 10);
 
 -- Función que devuelve el nombre de un cliente
 
@@ -91,7 +86,7 @@ end
 &&
 
 delimiter ;
-select buscar_nombre_cliente(1);
+select buscar_nombre_cliente(3);
 
 -- 3.2 Procedimientos
 
@@ -108,7 +103,7 @@ END
 &&
 
 delimiter ;
-call total_clientes_letra('e');
+call total_clientes_letra('q');
 
 
 -- devuelve el numero de pedidos y reservas realizados por el cliente
@@ -119,11 +114,12 @@ delimiter &&
 CREATE PROCEDURE reservas_pedidos_cliente(IN cod int)
 BEGIN
 
-	declare reserva int default(select count(c.CLIENTE_codigo_cliente) from clientehacereserva c 
-	where c.CLIENTE_codigo_cliente = cod);
+	declare reserva int default(select count(r.Cliente_codigo_cliente) from reserva r 
+	where r.Cliente_codigo_cliente = cod);
 	
-	declare pedido int default(select count(c.CLIENTE_codigo_cliente) from clientehacepedido c 
-	where c.CLIENTE_codigo_cliente = cod);
+	declare pedido int default(select count(p.Cliente_codigo_cliente) from pedido p
+	where p.Cliente_codigo_cliente = cod);
+	
 	
 	SELECT concat('El cliente ', buscar_nombre_cliente(cod), ' tiene ', (reserva + pedido), ' reservas y pedidos en total.') 
 	FROM cliente c 
@@ -133,36 +129,44 @@ END
 &&
 
 delimiter ;
-call reservas_pedidos_cliente(2);
-
--- inserta en una tabla llamada 'reservas_hechas_mes' donde almacena el numero de reservas hechas hoy
+call reservas_pedidos_cliente(571);
 
 
-drop table if exists reserva_mas_reciente;
-create table reserva_mas_reciente(
-	numeros_reservas int,
-	fecha_reciente_reserva datetime);
+-- inserta en una tabla llamada 'reserva_mes_especifico' donde almacena el número de reservas hechas en un mes
 
 
-DROP PROCEDURE IF EXISTS mete_reserva_reciente;
+DROP TABLE IF EXISTS reserva_mes_especifico;
+
+CREATE TABLE reserva_mes_especifico (
+  numeros_reservas INT,
+  nombre_mes VARCHAR(45)
+);
+
+DROP PROCEDURE IF EXISTS mete_reserva_mes;
 
 DELIMITER &&
-CREATE PROCEDURE mete_reserva_reciente()
+
+CREATE PROCEDURE mete_reserva_mes(IN mes VARCHAR(45))
 BEGIN
   DECLARE done INT DEFAULT FALSE;
+  DECLARE total_personas INT;
+  DECLARE mes_nombre VARCHAR(45);
   
-  declare a int;
-  DECLARE cliente_mes CURSOR FOR SELECT c.CLIENTE_codigo_cliente FROM clientehacereserva c 
- 						inner join reserva r ON c.RESERVA_numero_identificacion = r.numero_identificacion 
- 						where month(r.fecha_inicio) = month(now());
+  DECLARE cliente_mes CURSOR FOR
+    SELECT COUNT(*) AS total_personas, MONTHNAME(r.fecha_inicio) AS mes_nombre
+    FROM reserva r
+    WHERE MONTHNAME(r.fecha_inicio) = mes
+    GROUP BY MONTHNAME(r.fecha_inicio);
+
   DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
   OPEN cliente_mes;
 
   read_loop: LOOP
-    FETCH cliente_mes INTO a;
-    IF done then
-      INSERT INTO ataviada.reserva_mas_reciente VALUES (a, now());
+    FETCH cliente_mes INTO total_personas, mes_nombre;
+    IF done THEN
+      INSERT INTO reserva_mes_especifico (numeros_reservas, nombre_mes)
+      VALUES (total_personas, mes_nombre);
       LEAVE read_loop;
     END IF;
   END LOOP;
@@ -172,9 +176,11 @@ END
 &&
 
 DELIMITER ;
-CALL mete_reserva_reciente();
 
-SELECT * FROM reserva_mas_reciente;
+CALL mete_reserva_mes('May');
+
+SELECT * FROM reserva_mes_especifico;
+
 
 -- 4. Triggers
 
